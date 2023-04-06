c08aff13d539a6e515ddd3a811bb34e06f08996c
GioJibladze <130065832+GioJibladze@users.noreply.github.com>
2023-04-06 17:48:28 +0400

    github

---
+++ Eureka/js/mine.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('.navbar-toggler')
+let inp = document.querySelector('.heading')
+
+btn.addEventListener('click', function() {
+  inp.classList.toggle('new');
+
+})
+
+
